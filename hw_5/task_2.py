e94fba786ce9a7d5e4c8582dc914ed817fef1c4a
Elena Agapitova <agapitovae87@gmail.com>
2023-09-11 08:54:48 +0300

    hw_5 changed

--- hw_5/task_2.py
+++ hw_5/task_2.py
@@ -1,13 +1,20 @@
 # Напишите функцию, которая принимает на вход строку —
 # абсолютный путь до файла. Функция возвращает кортеж из трёх
 # элементов: путь, имя файла, расширение файла
+import os
+from typing import Any
 
 str_ = 'C:/Users/User/Desktop/Developer/FinalControl/linux.pdf'
 
 
-def tuple_str(txt):
+def split_path_1(txt):
     *a, b = txt.split('/')
     return '/'.join(a), *b.split('.')
 
 
-print(tuple_str(str_))
+def split_path_2(path: str) -> tuple[str, Any]:
+    return os.path.split(path)[0], *os.path.split(path)[1].split('.')
+
+
+print(split_path_1(str_))
+print(split_path_2(input('Введите путь к файлу: ')))
